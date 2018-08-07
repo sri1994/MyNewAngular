4a0ca803442682c608e5b35aa1b210bfe1eff103
srinivas prasad <srinivasjamadagni94@gmail.com>
2018-08-08 01:03:01 +0530

    Implemented UI for signin and sample home page

--- first-web-app/first-web-app/src/app/app.module.ts
+++ first-web-app/first-web-app/src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { ClarityModule } from "@clr/angular";
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: 'login', component: LoginPageComponent},
+  { path: 'home', component: HomePageComponent},
+  { path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +23,12 @@ import { HomePageComponent } from './home-page/home-page.component';
     HomePageComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserModule,
+    ClarityModule
   ],
   providers: [],
   bootstrap: [AppComponent]
